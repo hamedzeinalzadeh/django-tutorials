f63d7209d304e34c21bbc45857e23d7e9915cf6c
Hamed Zeinalzadeh <hamedzeinal@yahoo.com>
2022-08-03 09:46:42 +0430

    Dynamic routing and path converter

--- 9-django_views_routing_urls/src/first_app/urls.py
+++ 9-django_views_routing_urls/src/first_app/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from src.first_app import views
 
 # domain.com/first_app/ 
-url_patterns = [
-    path('', views.simple_view)
+urlpatterns = [
+    path('<str:topic>/', views.news_view),
+    path('<int:num1>/<int:num2>/', views.add_view)
 ]
--- 9-django_views_routing_urls/src/first_app/views.py
+++ 9-django_views_routing_urls/src/first_app/views.py
@@ -3,5 +3,18 @@ from django.shortcuts import render
 
 
 # Create your views here.
-def simple_view(request):
-    return HttpResponse('This is a simple view!')
+articles = {
+    'sports': 'Sports page',
+    'finance': 'Finance page',
+    'politics': 'Politics page'
+}
+
+def news_view(request, topic):
+    return HttpResponse(articles[topic])
+
+def add_view(request, num1, num2):
+    #domain.com/first_app/num1/num2
+    add_result = num1 + num2
+    result = f"{num1} + {num2} = {add_result}"
+    return HttpResponse(result)    
+
