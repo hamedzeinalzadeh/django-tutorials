0959ba3db53f823df1047fc9785dbe19a55dca72
Hamed Zeinalzadeh <hamedzeinal@yahoo.com>
2022-08-08 20:31:29 +0430

    Reverse url and urls name

--- 9-django_views_routing_urls/src/first_app/urls.py
+++ 9-django_views_routing_urls/src/first_app/urls.py
@@ -3,6 +3,9 @@ from src.first_app import views
 
 # domain.com/first_app/ 
 urlpatterns = [
-    path('<str:topic>/', views.news_view),
-    path('<int:num1>/<int:num2>/', views.add_view)
+
+    path('<int:num_page>', views.num_page_view),
+    path('<str:topic>/', views.news_view, name='topic-page'),
+    # path('<int:num1>/<int:num2>/', views.add_view)
+
 ]
--- 9-django_views_routing_urls/src/first_app/views.py
+++ 9-django_views_routing_urls/src/first_app/views.py
@@ -1,5 +1,8 @@
+from ast import arg
+from django.http import HttpResponseRedirect
 from django.http.response import HttpResponse, HttpResponseNotFound, Http404
 from django.shortcuts import render
+from django.urls import reverse
 
 
 # Create your views here.
@@ -18,9 +21,16 @@ def news_view(request, topic):
         #return HttpResponseNotFound(result) --> for debugging
         raise Http404('404 GENERIC ERROR')   #404.html 
 
-def add_view(request, num1, num2):
-    #domain.com/first_app/num1/num2
-    add_result = num1 + num2
-    result = f"{num1} + {num2} = {add_result}"
-    return HttpResponse(result)    
+# def add_view(request, num1, num2):
+#     #domain.com/first_app/num1/num2
+#     add_result = num1 + num2
+#     result = f"{num1} + {num2} = {add_result}"
+#     return HttpResponse(result)    
 
+#domain.com/first_app/0 --> domain.com/first_app/sports
+def num_page_view(request, num_page):
+
+    topics_list = list(articles.keys())
+    topic = topics_list[num_page]
+
+    return HttpResponseRedirect(reverse('topic-page', args=[topic]))
