cc5a5b869280169958418e8502fdd902ba1684dd
Hamed Zeinalzadeh <hamedzeinal@yahoo.com>
2022-08-15 23:22:20 +0430

    initial if tags

--- 10-Django-templates/src/my_app/views.py
+++ 10-Django-templates/src/my_app/views.py
@@ -8,7 +8,7 @@ def example_view(request):
 def variable_view(request):
 
     my_var = {'first_name': 'Rosalind', 'last_name': 'Franklin', 
-    'some_list': [1, 2, 3], 'some_dictionary': {'inside_key': 'inside_value'}
+    'some_list': [1, 2, 3], 'some_dictionary': {'inside_key': 'inside_value'}, 'user_logged_in': True
     }
 
     return render(request, 'my_app/variable.html', context=my_var)
