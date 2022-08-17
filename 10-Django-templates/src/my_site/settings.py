e93897183a848de3a97efb4e99639347c2db4d8a
Hamed Zeinalzadeh <hamedzeinal@yahoo.com>
2022-08-17 10:56:33 +0430

    custom error templates

--- 10-Django-templates/src/my_site/settings.py
+++ 10-Django-templates/src/my_site/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-x5b1&dglv0*4y(_&sq3=2n%)(ka4wb-9p@&4*v8&xjmq8v6&j5'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition
--- 10-Django-templates/src/my_site/urls.py
+++ 10-Django-templates/src/my_site/urls.py
@@ -13,6 +13,7 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from cgitb import handler
 from django.contrib import admin
 from django.urls import path, include
 
@@ -20,3 +21,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('my_app/', include('my_app.urls'))
 ]
+
+handler404 = 'src.my_site.view.my_custom_page_not_found_view'
